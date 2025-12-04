6372f77883bf06b237d83da08ac0bf72da0d8ee7
Joe Burgess <joeezmail@gmail.com>
2025-12-03 19:33:15 -0800

    Set invoice routes to admin or techs only

--- app/blueprints/invoice/routes.py
+++ app/blueprints/invoice/routes.py
@@ -4,15 +4,14 @@ from app.blueprints.invoice import invoices_bp
 from flask import jsonify, request
 from marshmallow import ValidationError
 from app.extensions import limiter, cache
-from werkzeug.security import check_password_hash, generate_password_hash
-from app.util.auth import encode_token, token_required
+from app.util.auth import admin_or_tech_token_required
 
 
 # INVOICE ROUTES
 # create invoice
 @invoices_bp.route("", methods=["POST"])
 @limiter.limit("5 per day")
-@token_required
+@admin_or_tech_token_required
 def create_invoice():
     try:
         data = invoice_schema.load(request.json)
@@ -28,7 +27,7 @@ def create_invoice():
 # get invoice by id
 @invoices_bp.route("/<int:id>", methods=["GET"])
 @limiter.limit("200 per day")
-@token_required
+@admin_or_tech_token_required
 def get_invoice(id):
     invoice = db.session.get(Invoice, id)
     return jsonify(invoice_schema.dump(invoice)), 200
@@ -38,7 +37,7 @@ def get_invoice(id):
 @invoices_bp.route("", methods=["GET"])
 @limiter.limit("200 per day")
 @cache.cached(timeout=500)
-@token_required
+@admin_or_tech_token_required
 def get_invoices():
     invoices = db.session.query(Invoice).all()
     return jsonify(invoices_schema.dump(invoices)), 200
@@ -47,7 +46,7 @@ def get_invoices():
 # delete invoices by id
 @invoices_bp.route("/<int:id>", methods=["DELETE"])
 @limiter.limit("5 per day")
-@token_required
+@admin_or_tech_token_required
 def delete_invoice(id):
     invoice = db.session.get(Invoice, id)
     if not invoice:
@@ -61,7 +60,7 @@ def delete_invoice(id):
 # update invoice by id
 @invoices_bp.route("/<int:id>", methods=["PUT"])
 @limiter.limit("10 per day")
-@token_required
+@admin_or_tech_token_required
 def update_invoice(id):
     invoice = db.session.get(Invoice, id)
 
