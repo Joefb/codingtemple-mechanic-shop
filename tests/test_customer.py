05ef8717fc1b27fc67aa309561e13954ca3c7a3f
Joe Burgess <joeezmail@gmail.com>
2025-12-05 03:34:29 -0800

    Add tests dir,add first tests, commented out code in __init__.py because of error, will fix later

---
+++ tests/test_customer.py
@@ -0,0 +1,60 @@
+from app import create_app
+from app.models import Customer, Tech, db
+import unittest
+from werkzeug.security import check_password_hash, generate_password_hash
+from app.util.auth import encode_token
+
+
+class TestCustomer(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app("TestingConfig")
+        self.customer = Customer(
+            first_name="Firstname",
+            last_name="Lastname",
+            email="test@test.com",
+            password=generate_password_hash("password"),
+            phone="123-456-7890",
+            address="123 Test St",
+        )
+
+        # tried this....
+        # self.admin_tech = Tech(
+        #     first_name="admin",
+        #     last_name="admin",
+        #     position="admin",
+        #     phone="000-000-0000",
+        #     password=generate_password_hash("password"),
+        # )
+
+        with self.app.app_context():
+            db.drop_all()
+            db.create_all()
+            db.session.add(self.customer)
+            # added this...
+            # db.session.add(self.admin_tech)
+            db.session.commit()
+
+        # admin postion is required to create customers. Is this right?
+        self.token = encode_token(1, "admin")
+        # self.token = encode_token(1)
+        self.client = self.app.test_client()
+
+    # create customer test
+    def test_create_customer(self):
+        customer_payload = {
+            "first_name": "TestBob",
+            "last_name": "TestBarker",
+            "email": "bob@bob.com",
+            "password": "bob",
+            "phone": "555-555-5555",
+            "address": "555 Bob St",
+        }
+
+        response = self.client.post(
+            "/customer",
+            json=customer_payload,
+            headers={"Authorization": f"Bearer {self.token}"},
+        )
+        self.assertEqual(response.status_code, 201)
+
+    # get current logged in customer
